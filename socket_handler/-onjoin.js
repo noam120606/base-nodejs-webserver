8fe6b8e6596ddfe0150a51c2168e01ae5b2256fe
noam120606 <54764246+noam120606@users.noreply.github.com>
2024-06-07 11:41:05 +0200

    Delete socket_handler directory

--- socket_handler/-onjoin.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (socket) => {
-    socket.rcon.on('response', async response => {
-        const players = response.split(":")[1];
-        if (!players) return;
-        let list = players.split(', ');
-        if (list.length>0) list[0] = list[0].substring(1)
-        socket.emit('playerList', list);
-    });
-};
--- socket_handler/ban.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: "ban",
-    execute: (socket, data) => {
-        // ban quelqu'un
-    }
-}
