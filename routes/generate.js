4fe0e4fd0b49af93e5c84c9b26617c474a27b69f
noam120606 <54764246+noam120606@users.noreply.github.com>
2024-06-07 11:40:56 +0200

    Delete routes directory

--- routes/generate.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    url: "/generate",
-    protocol: "GET",
-    run: (app, req, res) => {
-        if (!req.session.auth) return res.redirect('/login');
-        res.render('generate');
-    },
-};
--- routes/root.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    url: "/",
-    protocol: "GET",
-    run: (app, req, res) => {
-        if (!req.session.auth) return res.redirect('/login');
-        res.render('index');
-    },
-};
